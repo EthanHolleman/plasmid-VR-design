6ce08d26dbf6b649c4c594fe70f85be54e2e718a
Your Name <etholleman@ucdavis.edu>
2021-06-11 16:20:54 -0700

    build out sequence calculations

--- scripts/variable_region_maker.py
+++ scripts/variable_region_maker.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from utils import find_available_random_range 
 
 BRUTE_FORCE_CACHE = {}
 
@@ -14,8 +17,119 @@ def brute_force_gc_count_calculator(gc_skew, gc_content, seq_len, closest=True):
     pass
 
 
+class Sequence():
+    
+    def __init__(self, count_dict, cluster_length=None, cluster_nuc=None,
+                cluster_dist_func=None):
+        self.count_dict = count_dict
+        self.cluster_length = cluster_length
+        self.cluster_nuc = cluster_nuc
+        self.cluster_dist_func = cluster_dist_func
+        self.nuc_seq = self._make_sequence()
+
+        if self.cluster_length:
+            assert cluster_nuc, 'Must specify cluster nuc if passing cluster len!'
+            assert cluster_nuc in count_dict
+    
+
+    @cluster_dict_func.setter
+    def cluster_dict_func(self, new_func):
+        if callable(new_func):
+            self._cluster_dict_func = new_func
+        else:
+            return find_available_random_range
+
+
+    @property
+    def cluster_dict_func(self):
+        return self._cluster_dict_func
+
+
+    @property
+    def __len__(self):
+        return sum(self.count_dict.values())
+    
+
+    def _make_sequence(self):
+        '''Private method that actually generates the nucleotide string for
+        the Sequence instance. Should be called once when the instance is
+        created as everything needed to make it is known during initiation.
+
+        Returns:
+            str: Nucleotide sequence as a string.
+        '''
+        bins = [0 for _ in range(len(self))]
+        if cluster_length:
+            bins = self._distribute_nucleotides_with_clustering(bins)
+            pass
+        else:
+            bins = self._randomly_distribute_all_nucleotides(bins)
+        
+        return ''.join(bins)
+    
+
+    def _distribute_nucleotides_with_clustering(self, bins):
+        '''Fill nucleotide bins, but cluster together the nucleotide
+        specified by cluster_nuc in groups of size of cluster_length. 
+
+        Args:
+            bins (list): Empty list that represent the slots which are filled
+            with nucleotide symbols.
+
+        Returns:
+            list: Bins populated with clusters of nucleotide specified by
+            cluster_nuc.
+        '''
+        number_clusters = int(self.count_dict[self.cluster_nuc] / self.cluster_length)
+        for i in range(number_clusters):
+            rand_range = self.cluster_dist_func(bins, self.cluster_length)
+            if rand_range:
+                start, end = rand_range
+                bins[start, end] = self.cluster_nuc
+        
+        return bins
+
+
+    def _randomly_distribute_all_nucleotides(self, bins, ignore_nucs=[]):
+        '''Most basic form of sequence creation, uses the nucleotide symbols
+        and respective counts in count_dict to randomly fill in "bins"
+        (empty slots where nucleotides go).
+
+        Args:
+            bins (list): Empty list that represent the slots which are filled
+            with nucleotide symbols.
+            ignore_nucs (list): Do not add any nucleotides specified by this
+            list to bins. This is useful is a particular nucleotide as already
+            been added and you want to prevent adding twice. 
+
+        Returns:
+            list: List filled with randomly distributed nucleotides.
+        '''
+        available_bins = np.where(np.any(bins==0, axis=1))
+
+        random_bins = list(np.random.choice(
+                available_bins, len(available_bins), replace=False)
+                )
+        
+        temp_count_dict = self.count_dict
+        for nuc in ignore_nucs:
+            temp_count_dict.pop(nuc)
+
+        for nucleotide, count in self.count_dict.values():
+            for _ in range(count):
+                bins[random_bins.pop()] = nucleotide
+        return bins
+
+
+            
+
+
+
+
+
 
 class VairableRegion():
+    # defines the content of a Sequence
 
     name_prefix = 'VR'  # variable region
 
@@ -58,11 +172,3 @@ class VairableRegion():
 
 
 
-class ClusteredVariableRegion():
-        
-        def __init__(self, cluster_length, cluster_nucleotide, length, 
-                    gc_content, gc_skew, at_skew=None, at_content=None):
-            super().__init__(length, gc_content, gc_skew, at_skew, at_content)
-            self.cluster_length = cluster_length
-            self.cluster_nucleotide = cluster_nucleotide
-        
