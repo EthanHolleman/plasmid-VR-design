4fd8680ca25e259b8372963d4c53a01e572a365b
Your Name <etholleman@ucdavis.edu>
2021-06-18 11:58:53 -0700

    add script for handling weird file output names of SPOT RNA

---
+++ scripts/truncate_rename.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+from pathlib import Path
+
+
+# rename all files in a given directory by splitting filenames by a delimiter
+# and then selecting one of the remaining regions by index (base 0)
+# keeps the same file extension. 
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('DIR', help='Path to directory with files to truncate.')
+    parser.add_argument('--delim', default='_', help='Delimiter to split files by.')
+    parser.add_argument('--index', default=0, type=int, help='Index to keep after split')
+
+    return parser.parse_args()
+
+
+
+def main():
+    args = get_args()
+    for each_file in Path(args.DIR).iterdir():
+        ext = each_file.suffix
+        print(each_file, 'each file')
+        new_stem = each_file.stem.split(args.delim)[args.index]
+        new_name = new_stem + ext
+        each_file.rename(each_file.with_name(new_name))
+
+
+if __name__ == '__main__':
+    main()
+
+
