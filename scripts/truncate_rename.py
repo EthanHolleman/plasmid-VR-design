d90cbad7bafee48c24176e3c4c2a0cda17e60351
Your Name <etholleman@ucdavis.edu>
2021-06-19 13:12:00 -0700

    add arg for renaming all files to a constant stem but preserving suffix. Needed for random sequence RNA structure prediction

--- scripts/truncate_rename.py
+++ scripts/truncate_rename.py
@@ -12,6 +12,7 @@ def get_args():
     parser.add_argument('DIR', help='Path to directory with files to truncate.')
     parser.add_argument('--delim', default='_', help='Delimiter to split files by.')
     parser.add_argument('--index', default=0, type=int, help='Index to keep after split')
+    parser.add_argument('--constant_stem', default='', help='Set all file stems to this name but keep file extensions the same.')
 
     return parser.parse_args()
 
@@ -21,8 +22,10 @@ def main():
     args = get_args()
     for each_file in Path(args.DIR).iterdir():
         ext = each_file.suffix
-        print(each_file, 'each file')
-        new_stem = each_file.stem.split(args.delim)[args.index]
+        if args.constant_stem:
+            new_stem = args.constant_stem
+        else:
+            new_stem = each_file.stem.split(args.delim)[args.index]
         new_name = new_stem + ext
         each_file.rename(each_file.with_name(new_name))
 
