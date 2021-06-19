79993ebe94528b3299beba114ce42c5e0edfc357
Your Name <etholleman@ucdavis.edu>
2021-06-19 12:14:53 -0700

    debug and test bpRNA_parser

--- scripts/bpRNA_parser.py
+++ scripts/bpRNA_parser.py
@@ -1,6 +1,6 @@
 # script for parsing output of the bpRNA program into easily digestable
 # metrics for sequence comparison
-
+import os
 
 class StructDescrip():
 
@@ -21,26 +21,38 @@ class StructDescrip():
             number = split_line[0][1:]  # everything from beyond the code
             numbers = number.split('.')  # seperate primary and seconday id
             assert len(numbers) <= 2
+
             id = int(numbers[0])
             if len(numbers) == 2:
                 sec_id = int(numbers[1])
             else:
                 sec_id = None
-            return cls(code, id, sec_id)
+            
+            # try to get the range of structure (length)
+            struct_range = split_line[1].split('..')
+            # if not in this format then ignore
+            if len(struct_range) == 2:
+                struct_range = int(struct_range[0]), int(struct_range[1])
+            else:
+                struct_range = None
+
+            return cls(code, id, sec_id, struct_range)
         else:
             return None
 
-    def __init__(self, code, prim_id, sec_id=None):
+    def __init__(self, code, prim_id, sec_id=None, struct_range=None):
         self.code = code
         self.prim_id = prim_id
         self.sec_id = sec_id
+        self.struct_range = struct_range
     
 
     def __repr__(self):
         return ' '.join([f'{key}: {val}' for key, val in self.__dict__.items()])
 
-class StructureFile():
 
+
+class StructureFile():
 
     def __init__(self, filepath):
         self.filepath = filepath
@@ -56,46 +68,74 @@ class StructureFile():
     def _struct_descrip_parser(self, line):
         items = line.split(' ')
 
-
+    @property
+    def prop_unpaired(self):
+        return self.dot_bracket.count('.') / len(self)
     
+    @property
+    def num_hairpins(self):
+        return len(list(self._get_sd_of_type('H')))
+            
+        return count
+    
+    @property
+    def prop_hairpin(self):
+        total_hairpin_span = 0
+        for each_sd in self._get_sd_of_type('H'):
+            if each_sd.struct_range:
+                s, e = each_sd.struct_range
+                total_hairpin_span += (e - s)
+        return total_hairpin_span / len(self)
+
+
     def _read_file(self):
         headers = []
         body_lines = 0
         with open(self.filepath) as handle:
-            while handle:
-                cur_line = handle.readline()
-                if cur_line[0] == '#':
-                    headers.append(cur_line[1:].strip())
-                else:
-                    if body_lines == 0:
-                        # read the nucleotide sequence
-                        self.seq = cur_line.strip()
-                    elif body_lines == 1:
-                        self.dot_bracket = cur_line.strip()
-                    elif body_lines == 2:
-                        self.struct_str = cur_line.strip()
-                    else:
-                        self.struct_descrips.append(cur_line.strip())
-                    body_lines += 1
+            lines = handle.readlines()
+            for cur_line in lines:
+                if cur_line:
+                    if cur_line[0] == '#':  # header line
+                        headers.append(cur_line[1:].strip())
+                    else:  # body
+                        if body_lines == 0:
+                            self.header = headers
+                            self.seq = cur_line.strip()
+                        elif body_lines == 1:
+                            self.dot_bracket = cur_line.strip()
+                        elif body_lines == 2:
+                            self.struct_str = cur_line.strip()
+                        elif body_lines == 3:
+                            pass  # some weird string maybe related to knots?
+                        else:
+                            descrip =  StructDescrip.init_from_line(cur_line.strip())
+                            if descrip:
+                                self.struct_descrips.append(descrip)
+
+                        body_lines += 1
+
         
-        self.headers = headers
-    
     def _run_checks(self):
         assert len(self.seq) == len(self.dot_bracket) == len(self.struct_str)
     
     def __len__(self):
         return len(self.seq)
-
-    @property
-    def prop_paired(self):
-        unpaired = self.dot_bracket.count('.') / len(self)
     
-    @property
-    def number_of_hairpins(self):
-        count = 0
+    def _get_sd_of_type(self, code):
+        # return all StructureDescrip instances with the code specified
+        # by code argument
         for each_sd in self.struct_descrips:
-            if each_sd.code == 'H':
-                count += 1
-            
-        return count
-
+            if each_sd.code == code:
+                yield each_sd
+    
+    def to_tsv(self, output_path):
+        # headers would be
+        # filepath, prop_unpaired, length, num_hairpins, prop_hairpin
+        with open(output_path, 'w') as handle:
+            handle.write(
+                '\t'.join([str(item) for item in 
+                [self.filepath, self.prop_unpaired, len(self),
+                self.num_hairpins, self.prop_hairpin]])
+            )
+        return output_path
+        
