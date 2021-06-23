ac1b242820873b7ca5fe68a495bcfe517c1596af
Your Name <etholleman@ucdavis.edu>
2021-06-23 08:38:37 -0700

    work on adding more tests. Need to choose best way of getting test files

--- scripts/tests/test_agg_seq_metrics.py
+++ scripts/tests/test_agg_seq_metrics.py
@@ -1,2 +1,16 @@
-import pytset
+import pytest
 
+import os
+import sys
+from pathlib import Path
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from agg_seq_metrics import *
+
+# class Snakemake():
+#     def __init__(self **kwargs):
+#         self.__dict__.update(kwargs)
+
+# @pytest.fixture
+# def snakemake():  # simulate snakemake object
