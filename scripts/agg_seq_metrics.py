15f3dffdbb4ba3e51dae30e5704225432edb79c1
Your Name <etholleman@ucdavis.edu>
2021-06-24 09:24:17 -0700

    implement new sequence ranking method

--- scripts/agg_seq_metrics.py
+++ scripts/agg_seq_metrics.py
@@ -7,9 +7,8 @@ import pandas as pd
 import os
 
 class Expectation():
-
+    
     expectations = {}
-
 
     @classmethod
     def extend_expect_dict_from_table(cls, filepath, length):
@@ -29,15 +28,15 @@ class Expectation():
         self.expect_mean = float(expect_mean)
         self.expect_sd = float(expect_sd)
 
-    def distance(self, metric):
+
+    def z_score(self, metric):
         # distance in standard deviations from the mean
-        print(metric, self.expect_mean, 'EXPECTED MEAN')
         z = (metric.value - self.expect_mean) / self.expect_sd
-        print(metric.value, self.expect_mean, z, 'VAL, MEAN, Z')
         assert isinstance(z, float)
         
-        return f'distance_{self.name}',  z
+        return f'zscore_{self.name}',  z
         
+
     def __repr__(self):
         return ' '.join([f'{str(key)}:{str(val)}' for key, val in self.__dict__.items()])
     
@@ -46,64 +45,39 @@ class Metric():
 
     NAME = 'metric'
 
-    def __init__(self, filepath, expectation, direction, divergence):
+    def __init__(self, filepath, expectation):
         self.filepath = filepath
         self.expectation= expectation
-        self.direction = direction
-        self.divergence = divergence
 
         assert os.path.isfile(filepath)
         assert isinstance(expectation, Expectation)
 
-        print(self, expectation)
 
     @property
-    def distance(self):
-        dist_name, val = self.expectation.distance(self)
-        val = self._apply_scaling(val)
+    def z_score(self):
+        dist_name, val = self.expectation.z_score(self)
         return {dist_name: val}
     
     @property
     def value(self):
         return None
     
-    @property
-    def direction(self):
-        return self._direction
-    
-    @direction.setter
-    def direction(self, new_dir):
-        if new_dir == 1 or new_dir == -1:
-            self._direction = new_dir
-        else:
-            raise TypeError('Direction must be -1 or 1!')
-    
-    @property
-    def divergence(self):
-        return self._divergence
-    
-    @direction.setter
-    def divergence(self, new_dir):
-        if new_dir == 1 or new_dir == -1:
-            self._divergence = new_dir
-        else:
-            raise TypeError('Divergence must be -1 or 1!')
-    
+
     def parse(self):
         d = {
             type(self).NAME: self.value,  # original value
         }
-        d.update(self.distance)
+        d.update(self.z_score)
         return d
     
     def _apply_scaling(self, z):
-        return (z * self.direction) ** self.divergence
+        return (z * self.divergence) ** self.direction
 
 
 class rlooperMetric(Metric):
 
-    def __init__(self, filepath, expectation, direction, divergence):
-        super().__init__(filepath, expectation, direction, divergence)
+    def __init__(self, filepath, expectation):
+        super().__init__(filepath, expectation)
     
     def _parse_rlooper_wig(self):
         values = []
@@ -116,8 +90,8 @@ class bpProb(rlooperMetric):
     # basepair probability from rlooper
     NAME = 'bp_prob'
 
-    def __init__(self, filepath, expectation, direction, divergence):
-        super().__init__(filepath, expectation, direction, divergence)
+    def __init__(self, filepath, expectation):
+        super().__init__(filepath, expectation)
 
     @property
     def value(self):
@@ -128,8 +102,8 @@ class localAverageEnergy(rlooperMetric):
 
     NAME = 'local_average_energy'
 
-    def __init__(self, filepath, expectation, direction, divergence):
-        super().__init__(filepath, expectation, direction, divergence)
+    def __init__(self, filepath, expectation):
+        super().__init__(filepath, expectation)
 
     @property
     def value(self):
@@ -142,8 +116,8 @@ class rnaMetric(Metric):
                   'prop_unpaired': 1
                 }
 
-    def __init__(self, filepath, expectation, direction, divergence):
-        super().__init__(filepath, expectation, direction, divergence)
+    def __init__(self, filepath, expectation):
+        super().__init__(filepath, expectation)
     
 
     def _parse_rna_file(self):
@@ -160,8 +134,8 @@ class propHairpin(rnaMetric):
 
     NAME = 'prop_hairpin'
 
-    def __init__(self, filepath, expectation, direction, divergence):
-        super().__init__(filepath, expectation, direction, divergence)
+    def __init__(self, filepath, expectation):
+        super().__init__(filepath, expectation)
 
     @property
     def value(self):
@@ -173,8 +147,8 @@ class propUnpaired(rnaMetric):
     NAME = 'prop_unpaired'
 
 
-    def __init__(self, filepath, expectation, direction, divergence):
-        super().__init__(filepath, expectation, direction, divergence)
+    def __init__(self, filepath, expectation):
+        super().__init__(filepath, expectation)
     
     @property
     def value(self):
@@ -187,10 +161,6 @@ def add_inputs_to_out_dict(out_dict, snakemake):
     out_dict['length'] = snakemake.params['length']
     out_dict['p_name'] = snakemake.params['p_name']
     out_dict['id_num'] = snakemake.params['id_num']
-
-    # out_dict['fasta'] = 'output/initiation_regions/files/init-1/10/init-1.10.fasta'
-    # out_dict['tsv'] = 'output/initiation_regions/files/init-1/10/init-1.10.tsv'
-    # out_dict['length'] = 200
 
     return out_dict
 
@@ -219,28 +189,20 @@ def main():
 
     output_path = str(snakemake.output)
 
-    expect_def_dict = snakemake.params['config_dict']['EXPECTATION_DEFS']
-
     # initialize expectation instances from expect files
     Expectation.extend_expect_dict_from_table(rlooper_expect_path, length)
     Expectation.extend_expect_dict_from_table(rna_expect_path, length)
 
-    
     metrics = [
         bpProb(
-            input_bp_prob, Expectation.expectations[bpProb.NAME],
-            **expect_def_dict[bpProb.NAME]),
+            input_bp_prob, Expectation.expectations[bpProb.NAME]),
         localAverageEnergy(
-            input_lae, Expectation.expectations[localAverageEnergy.NAME],
-            **expect_def_dict[localAverageEnergy.NAME]),
+            input_lae, Expectation.expectations[localAverageEnergy.NAME]),
         propHairpin(
-            input_rna, Expectation.expectations[propHairpin.NAME],
-            **expect_def_dict[propHairpin.NAME]),
+            input_rna, Expectation.expectations[propHairpin.NAME]),
         propUnpaired(
-            input_rna, Expectation.expectations[propUnpaired.NAME],
-            **expect_def_dict[propUnpaired.NAME])
+            input_rna, Expectation.expectations[propUnpaired.NAME])
     ]
-
 
     out_dict = {}
 
--- scripts/rank_seqs.py
+++ scripts/rank_seqs.py
@@ -6,42 +6,98 @@ from shutil import copyfile
 def read_metrics_file(filepath):
     return pd.read_table(str(filepath), sep='\t').to_dict('records')
 
-def write_ranker_list_to_file(output_path, sorted_ranker_list):
-
-    with open(str(output_path), 'w') as handle:
-        headers = list(sorted_ranker_list[0].__dict__.keys())
-        writer = csv.DictWriter(handle, delimiter='\t', fieldnames=headers)
-        writer.writeheader()
-        for rank in sorted_ranker_list:
-            rank_dict = rank.__dict__
-            print(rank_dict)
-            writer.writerow(rank_dict)
-        return output_path
-
 
 def copy_top_rank_tsv_fasta(top_ranker, fasta_output, tsv_output):
+    print(top_ranker)
     assert top_ranker.rank == 0
-    copyfile(top_ranker.fasta, fasta_output)
-    copyfile(top_ranker.tsv, tsv_output)
+    copyfile(top_ranker.seq_data['fasta'], fasta_output)
+    copyfile(top_ranker.seq_data['tsv'], tsv_output)
 
 
+class Sequence():
 
-class Ranker():
 
     def __init__(self, *args, **kwargs):
-        self.__dict__.update(kwargs)
+        self.seq_data = kwargs
+        self.rank_dict = {}
         self.rank = None
     
     @property
-    def overall_fit(self):
-        distance = 0
-        for key, val in self.__dict__.items():
-            if 'distance' in key:
-                distance += float(val)
-        return distance
+    def zscore_dict(self):
+        # get dictionary of all metrics and zscores for this sequence
+        z_dict = {}
+        for attribute, val in self.seq_data.items():
+            print(attribute)
+            if 'zscore' in attribute:
+                print('ZSCORE IN ATTRIBUTE')
+                attribute_name = attribute.replace('zscore_', '')
+                z_dict[attribute_name] = float(val)
+
+        assert z_dict
+        return z_dict
     
-    def __lt__(self, other_ranker):
-        return self.overall_fit < other_ranker.overall_fit
+    @property
+    def overall_distance(self):
+        # sum of all ranks
+        return sum(self.rank_dict.values())
+    
+    def to_dict(self):
+        labeled_rank_dict = {}
+        labeled_rank_dict = {f'rank_{attribute}': rank for attribute, rank in self.rank_dict.items()}
+        labeled_rank_dict.update(self.seq_data)
+        labeled_rank_dict.update({'overall_rank': self.rank})
+        labeled_rank_dict.update({'overall_distance': self.overall_distance})
+        print(self.rank_dict, 'rank dict')
+        return labeled_rank_dict
+    
+    def __lt__(self, other_seq):
+        self.overall_distance < other_seq.overall_distance
+        
+
+class Ranker():
+
+    def __init__(self, sequences):
+        self.sequences = sequences
+     
+    def rank_sequences_by_metric(self, attribute, direction, divergence):
+        print('%'*10)
+        print('ATTRIBUTE', attribute)
+        assert direction == 1 or direction == -1
+        assert divergence == 1 or divergence == -1
+
+        if divergence == 1:  # minimize distance to mean don't care about direction
+            sorted_seqs = sorted(
+                self.sequences, 
+                key=lambda s: abs(s.zscore_dict[attribute])
+                )
+        else:  # maximize distance to mean
+            if direction == 1:  # desired value right of mean
+                sorted_seqs = sorted(
+                    self.sequences, 
+                    key=lambda s: s.zscore_dict[attribute]
+                    )
+            else:  # left of mean
+                sorted_seqs = sorted(
+                self.sequences,
+                key=lambda s: s.zscore_dict[attribute], revese=True
+                )
+            
+        # assign rank to attribute
+        for i, each_seq in enumerate(sorted_seqs):
+            sorted_seqs[i].rank_dict[attribute] = i
+        
+        self.sequences = sorted_seqs
+
+
+def get_metrics(dict_list):
+    metrics = []
+    keys = list(dict_list[0].keys())
+    for attribute in keys:
+        if 'zscore_' in attribute:
+            attribute_name = attribute.replace('zscore_', '')
+            metrics.append(attribute_name)
+    
+    return metrics
 
 
 def main():
@@ -51,14 +107,28 @@ def main():
     output_path_fasta = str(snakemake.output['fasta'])
     output_path_tsv = str(snakemake.output['tsv'])
 
+    expect_def_dict = snakemake.params['config_dict']['EXPECTATION_DEFS']
+
     dict_list = read_metrics_file(input_path)
-    ranker_list = [Ranker(**row_dict) for row_dict in dict_list]
-    ranker_list = sorted(ranker_list, reverse=True)  # highest value first = best seq first
-    for i, ranker in enumerate(ranker_list):
-        ranker.rank = i
-        ranker_list[i] = ranker
-    write_ranker_list_to_file(output_path_ranked, ranker_list)
-    copy_top_rank_tsv_fasta(ranker_list[0], output_path_fasta, output_path_tsv)
+
+    seqs = [Sequence(**row_dict) for row_dict in dict_list]
+    ranker = Ranker(seqs)
+
+    metrics = get_metrics(dict_list)
+
+    for metric in metrics:
+        print(metric, 'thIS IS IS METRIC')
+        ranker.rank_sequences_by_metric(metric, **expect_def_dict[metric])
+    
+    ranked_seqs = sorted(ranker.sequences, key=lambda x: x.overall_distance)
+    for i, seq in enumerate(ranked_seqs):
+        ranked_seqs[i].rank = i
+    
+    ranked_seq_dicts = [seq.to_dict() for seq in ranked_seqs]
+
+    pd.DataFrame(ranked_seq_dicts).to_csv(output_path_ranked, sep='\t', index=False)
+
+    copy_top_rank_tsv_fasta(ranked_seqs[0], output_path_fasta, output_path_tsv)
 
 
 if __name__ == '__main__':
