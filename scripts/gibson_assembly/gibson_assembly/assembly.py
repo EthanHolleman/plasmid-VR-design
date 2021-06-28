96a51324c15d8dec0594213342e1129b5d42e215
Your Name <etholleman@ucdavis.edu>
2021-06-28 16:43:08 -0700

    Add notes about what Gibson asseembler should do

--- scripts/gibson_assembly/gibson_assembly/assembly.py
+++ scripts/gibson_assembly/gibson_assembly/assembly.py
@@ -5,4 +5,8 @@ class GibsonAssembler():
     def __init__(self, construct):
         self.construct = construct
     
-    
+    # this object should take over all assembly functions
+    # also should implement reverse complmentation of inserted
+    # regions if required as that is currently not implemented
+    # additionally should search for regions of existing homology
+    # before converting seqrecords to amplicon instances.
