7687f00413142d70182cd6f30100a1cc93f0b3df
Your Name <etholleman@ucdavis.edu>
2021-07-04 10:21:35 -0700

    reduce mas jobs per second to 1

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 mkdir -p logs
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate snakemake
-snakemake -j 24 --cluster-config cluster.yml \
+snakemake -j 100 --cluster-config cluster.yml \
 --cluster "sbatch -p {cluster.partition} -t {cluster.time} -N {cluster.nodes} \
 -n {cluster.cpus} --mem {cluster.mem} -J {cluster.name} -o {cluster.output} \
 -e {cluster.output} --mail-type ALL --mail-user {cluster.email}" \
 --conda-frontend=mamba --configfile config.yml \
---latency-wait 60 --verbose --use-conda --rerun-incomplete
+--latency-wait 60 --verbose --use-conda --rerun-incomplete \
+--max-jobs-per-second 1
